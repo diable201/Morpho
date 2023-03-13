4e95ffcd5db05a4390bb5305bfc388ad812c843d
Seitbekov Sanzhar <diable201@protonmail.com>
2023-03-13 19:47:58 +0600

    Logging

--- main.py
+++ main.py
@@ -5,10 +5,26 @@ import telebot
 import time
 import pymongo
 import json
+import sys
 from telebot import types
 from typing import Callable
 from dotenv import load_dotenv, find_dotenv
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('logs.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+
+logger.addHandler(file_handler)
+logger.addHandler(stdout_handler)
 
 load_dotenv(find_dotenv())
 # Constants
